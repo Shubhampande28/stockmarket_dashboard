75114466ff6de0f8454c311e75d241c5b787db1b
Saloni Pande <pandesaloni2@gmail.com>
2026-05-01 16:05:30 +0530

    Use square heatmap tiles on phone

--- frontend/script.js
+++ frontend/script.js
@@ -211,16 +211,16 @@ function getMovementStyleBase(intensity, index) {
 function getMosaicStyle(stock, intensity) {
     if (window.innerWidth <= 420) {
         return {
-            spanX: 1,
-            spanY: 1,
+            spanX: intensity > 0.72 ? 2 : 1,
+            spanY: intensity > 0.72 ? 2 : 1,
             textBoost: "0px"
         };
     }
 
     if (window.innerWidth <= 640) {
         return {
-            spanX: 1,
-            spanY: 1,
+            spanX: intensity > 0.72 ? 2 : 1,
+            spanY: intensity > 0.72 ? 2 : 1,
             textBoost: "0px"
         };
     }
